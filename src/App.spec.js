a9b6b85081fff49803f8d37a9759a63720e7c946
Gizelly Silva <gizelly.silva@playkids.com>
2022-03-01 23:20:08 -0400

    feat: testing functions

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 
-import App from './App'
+import App, { calcularNovoSaldo } from './App'
 
-describe('AppComponent', () => {
+describe('AppComponent - Displaying info from bank', () => {
+   
    it('should be display bank name', () => {
       render(<App />);
 
@@ -21,4 +22,27 @@ describe('AppComponent', () => {
 
       expect(screen.getByText('Realizar operação')).toBeInTheDocument()
    })
+
+});
+
+describe('AppComponent - Carrying out bank transactions', () => {
+
+   it('should be increase balance when occur a deposit', () => {
+      const values = {
+         transacao: 'deposito',
+         valor: 300
+      }
+      const newBalance = calcularNovoSaldo(values, 1000)
+      expect(newBalance).toBe(1300);
+   });
+
+   it('should be decrease balance when occur a withdrawal', () => {
+      const values = {
+         transacao: 'saque',
+         valor: 300
+      }
+      const newBalance = calcularNovoSaldo(values, 1000)
+      expect(newBalance).toBe(700);
+   });
+
 });
